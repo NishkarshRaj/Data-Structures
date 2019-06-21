84939541ab7842a9bd59c88cc4a7b59c52a7a03f
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-06-21 14:49:25 +0530

    Inorder Traversal code for binary tree

---
+++ Trees/InorderTraversal.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class Tree
+{
+	public:
+	int key;
+	Tree *left;
+	Tree *right;
+	void insert()
+	{
+		cout<<"Insert: ";
+		cin>>key;
+	}
+	void show()
+	{
+		cout<<key<<endl;
+	}	
+};
+void print(Tree *node)
+{
+	if(node==NULL)
+	{
+		return;
+	}
+	print(node->left);
+	node->show();
+	print(node->right);
+}
+int main()
+{
+	Tree *root;
+	cout<<"InOrder Traversal of Binary Tree"<<endl;
+	cout<<"Enter Integer values when prompted"<<endl;
+	root = new Tree;
+	root->insert();
+	root->left=NULL;
+	root->right=NULL;
+	root->left = new Tree;
+	root->left->insert();
+	root->left->left=NULL;
+	root->left->right=NULL;
+	root->right=new Tree;
+	root->right->insert();
+	root->right->right=NULL;
+	root->right->left=NULL;
+	//insertion over
+	print(root);
+}
